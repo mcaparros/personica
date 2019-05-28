5dbb04ed243998c3876c24418a2db4533918d933
Manuel <Manuel@DESKTOP-JMBL8LC>
2019-05-28 16:37:29 +0200

    Añadiendo Javadoc

--- src/gitt/Persona.java
+++ src/gitt/Persona.java
@@ -1,14 +1,27 @@
 package gitt;
-
+/**
+ * Empieza la clase Persona
+ * @author Manuel
+ *
+ */
 public class Persona {
-	
+	/**
+	 * Atributos clase persona.
+	 */
 	private String nombre;
-
+	/**
+	 * Obtenemos el nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
-
+	/**
+	 * Asignamos un nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 * Acaba la clase Persona
+	 */
 }
